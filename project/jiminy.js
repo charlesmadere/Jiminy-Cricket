5656f62973620b992eee70b02fe8b810861e814a
Charles Madere <scootrnova@gmail.com>
2011-08-23 22:35:31 -0500

    second commit

---
+++ project/jiminy.js
@@ -0,0 +1,10 @@
+function randomBackground(inputID)
+// Randomly picks a Jiminy Cricket image as the background image
+// at the bottom right corner of the page.
+{
+	// the number of Jiminy images. They need to be named properly
+	// for this to work. "logo1.jpg", "logo2.jpg", ...
+	var imageCount = 3;
+
+	document.getElementById(inputID).style.backgroundImage = "url(images/logo" + Math.floor((Math.random() * imageCount) + 1) + ".jpg)";
+}
