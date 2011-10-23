bb276796e7753d7da8136e218f718a8fec27c698
Charles Madere <scootrnova@gmail.com>
2011-10-22 20:29:57 -0500

    preparation for ajax sending of canvas data

---
+++ defunct/wepaint.php
@@ -0,0 +1,137 @@
+<?php
+
+
+	require("facebook.php");
+
+	$facebook = new Facebook(array
+	(
+		'appId' => '211936785535748',
+		'secret' => '760f613bc10ba7bb970b3b4df4c1ff87',
+	));
+
+	// See if there is a user from a cookie
+	$user = $facebook->getUser();
+
+	if ($user)
+	{
+		try
+		{
+			// Proceed knowing you have a logged in user who's authenticated.
+			$user_profile = $facebook->api("/me");
+		}
+		catch (FacebookApiException $e)
+		{
+			echo "<pre>" . htmlspecialchars(print_r($e, true)) . "</pre>";
+			$user = null;
+		}
+	}
+
+
+	// Team Jiminy Cricket
+
+
+?>
+
+
+<!doctype html>
+
+
+<html lang="en">
+
+	<head>
+		<link href="assets/stylesheets/basic.css" rel="stylesheet" type="text/css" />
+		<link href="assets/stylesheets/facebook.css" rel="stylesheet" type="text/css" />
+		<link href="assets/stylesheets/paint.css" rel="stylesheet" type="text/css" />
+		<meta charset="UTF-8" />
+		<script src="assets/javascript/jquery.js" type="text/javascript"></script>
+		<script src="assets/javascript/modernizr.js" type="text/javascript"></script>
+		<script src="assets/javascript/debugger.js" type="text/javascript"></script>
+		<script src="assets/javascript/basic.js" type="text/javascript"></script>
+		<script src="assets/javascript/paint.js" type="text/javascript"></script>
+		<script type="text/javascript">
+			$(document).ready(function()
+			// main method
+			{
+				// initialize our paint controls and functions
+				paintCanvasInit();
+			});
+		</script>
+		<title>Let's Paint! ~ WePaint.us</title>
+	</head>
+
+	<body>
+		<div id="fb-root"></div>
+		<div id="header">
+			<img src="images/wepaint.png" alt="WePaint.us" />
+			<img src="images/nav/divider.png" />
+			<img src="images/nav/spacer.png" />
+		</div>
+		<div id="content">
+			<div id="contentLeft">
+				<div class="bottomBorder" id="currentWordAndTimeLeft">
+					currentWordAndTimeLeft
+				</div>
+				<div class="bottomBorder" id="paintArea">
+					<canvas id="paintCanvas" height="470" width="700"></canvas>
+				</div>
+				<div id="toolBox">
+					<table id="toolBoxTable">
+						<tr>
+							<td><img src="images/paint/colors/colorBlack.png" id="colorBlack" alt="Black" onmouseout="paintColorOnMouseOut('colorBlack')" onmouseover="paintColorOnMouseOver('colorBlack')" onclick="paintColorOnClick('colorBlack')" /></td>
+							<td><img src="images/paint/colors/colorGrey.png" id="colorGrey" alt="Grey" onmouseout="paintColorOnMouseOut('colorGrey')" onmouseover="paintColorOnMouseOver('colorGrey')" onclick="paintColorOnClick('colorGrey')" /></td>
+							<td><img src="images/paint/colors/colorMaroon.png" id="colorMaroon" alt="Maroon" onmouseout="paintColorOnMouseOut('colorMaroon')" onmouseover="paintColorOnMouseOver('colorMaroon')" onclick="paintColorOnClick('colorMaroon')" /></td>
+							<td><img src="images/paint/colors/colorRed.png" id="colorRed" alt="Red" onmouseout="paintColorOnMouseOut('colorRed')" onmouseover="paintColorOnMouseOver('colorRed')" onclick="paintColorOnClick('colorRed')" /></td>
+							<td><img src="images/paint/colors/colorOrange.png" id="colorOrange" alt="Orange" onmouseout="paintColorOnMouseOut('colorOrange')" onmouseover="paintColorOnMouseOver('colorOrange')" onclick="paintColorOnClick('colorOrange')" /></td>
+							<td><img src="images/paint/colors/colorYellow.png" id="colorYellow" alt="Yellow" onmouseout="paintColorOnMouseOut('colorYellow')" onmouseover="paintColorOnMouseOver('colorYellow')" onclick="paintColorOnClick('colorYellow')" /></td>
+							<td><img src="images/paint/colors/colorGreen.png" id="colorGreen" alt="Green" onmouseout="paintColorOnMouseOut('colorGreen')" onmouseover="paintColorOnMouseOver('colorGreen')" onclick="paintColorOnClick('colorGreen')" /></td>
+							<td><img src="images/paint/colors/colorLightBlue.png" id="colorLightBlue" alt="Light Blue" onmouseout="paintColorOnMouseOut('colorLightBlue')" onmouseover="paintColorOnMouseOver('colorLightBlue')" onclick="paintColorOnClick('colorLightBlue')" /></td>
+							<td><img src="images/paint/colors/colorBlue.png" id="colorBlue" alt="Blue" onmouseout="paintColorOnMouseOut('colorBlue')" onmouseover="paintColorOnMouseOver('colorBlue')" onclick="paintColorOnClick('colorBlue')" /></td>
+							<td><img src="images/paint/colors/colorPurple.png" id="colorPurple" alt="Purple" onmouseout="paintColorOnMouseOut('colorPurple')" onmouseover="paintColorOnMouseOver('colorPurple')" onclick="paintColorOnClick('colorPurple')" /></td>
+							<td rowspan="2"><img src="images/paint/tools/toolPencil.png" id="toolPencil" onmouseout="paintToolOnMouseOut('toolPencil')" onmouseover="paintToolOnMouseOver('toolPencil')" onclick="paintToolOnClick('toolPencil')" alt="Pencil Tool" /></td>
+							<td rowspan="2"><img src="images/paint/tools/toolBrush.png" id="toolBrush" onmouseout="paintToolOnMouseOut('toolBrush')" onmouseover="paintToolOnMouseOver('toolBrush')" onclick="paintToolOnClick('toolBrush')" alt="Brush Tool" /></td>
+							<td rowspan="2"><img src="images/paint/tools/toolBucket.png" id="toolBucket" onmouseout="paintToolOnMouseOut('toolBucket')" onmouseover="paintToolOnMouseOver('toolBucket')" onclick="paintToolOnClick('toolBucket')" alt="Bucket Tool" /></td>
+							<td rowspan="2"><img src="images/paint/tools/toolEraser.png" id="toolEraser" onmouseout="paintToolOnMouseOut('toolEraser')" onmouseover="paintToolOnMouseOver('toolEraser')" onclick="paintToolOnClick('toolEraser')" alt="Eraser Tool" /></td>
+							<td rowspan="2"><img src="images/paint/tools/toolUndo.png" id="toolUndo" onmouseout="paintToolOnMouseOut('toolUndo')" onmouseover="paintToolOnMouseOver('toolUndo')" onclick="paintToolOnClick('toolUndo')" alt="Undo Tool" /></td>
+							<td rowspan="2"><img src="images/paint/tools/toolNuke.png" id="toolNuke" onmouseout="paintToolOnMouseOut('toolNuke')" onmouseover="paintToolOnMouseOver('toolNuke')" onclick="paintToolOnClick('toolNuke')" alt="Nuke Tool" /></td>
+						</tr>
+						<tr>
+							<td><img src="images/paint/colors/colorWhite.png" id="colorWhite" alt="White" onmouseout="paintColorOnMouseOut('colorWhite')" onmouseover="paintColorOnMouseOver('colorWhite')" onclick="paintColorOnClick('colorWhite')" /></td>
+							<td><img src="images/paint/colors/colorLightGrey.png" id="colorLightGrey" alt="Light Grey" onmouseout="paintColorOnMouseOut('colorLightGrey')" onmouseover="paintColorOnMouseOver('colorLightGrey')" onclick="paintColorOnClick('colorLightGrey')" /></td>
+							<td><img src="images/paint/colors/colorBrown.png" id="colorBrown" alt="Brown" onmouseout="paintColorOnMouseOut('colorBrown')" onmouseover="paintColorOnMouseOver('colorBrown')" onclick="paintColorOnClick('colorBrown')" /></td>
+							<td><img src="images/paint/colors/colorPink.png" id="colorPink" alt="Pink" onmouseout="paintColorOnMouseOut('colorPink')" onmouseover="paintColorOnMouseOver('colorPink')" onclick="paintColorOnClick('colorPink')" /></td>
+							<td><img src="images/paint/colors/colorYellowOrange.png" id="colorYellowOrange" alt="Yellow Orange" onmouseout="paintColorOnMouseOut('colorYellowOrange')" onmouseover="paintColorOnMouseOver('colorYellowOrange')" onclick="paintColorOnClick('colorYellowOrange')" /></td>
+							<td><img src="images/paint/colors/colorTan.png" id="colorTan" alt="Tan" onmouseout="paintColorOnMouseOut('colorTan')" onmouseover="paintColorOnMouseOver('colorTan')" onclick="paintColorOnClick('colorTan')" /></td>
+							<td><img src="images/paint/colors/colorYellowGreen.png" id="colorYellowGreen" alt="Yellow Green" onmouseout="paintColorOnMouseOut('colorYellowGreen')" onmouseover="paintColorOnMouseOver('colorYellowGreen')" onclick="paintColorOnClick('colorYellowGreen')" /></td>
+							<td><img src="images/paint/colors/colorSkyBlue.png" id="colorSkyBlue" alt="Sky Blue" onmouseout="paintColorOnMouseOut('colorSkyBlue')" onmouseover="paintColorOnMouseOver('colorSkyBlue')" onclick="paintColorOnClick('colorSkyBlue')" /></td>
+							<td><img src="images/paint/colors/colorRoyalBlue.png" id="colorRoyalBlue" alt="Royal Blue" onmouseout="paintColorOnMouseOut('colorRoyalBlue')" onmouseover="paintColorOnMouseOver('colorRoyalBlue')" onclick="paintColorOnClick('colorRoyalBlue')" /></td>
+							<td><img src="images/paint/colors/colorLightPurple.png" id="colorLightPurple" alt="Light Purple" onmouseout="paintColorOnMouseOut('colorLightPurple')" onmouseover="paintColorOnMouseOver('colorLightPurple')" onclick="paintColorOnClick('colorLightPurple')" /></td>
+						</tr>
+					</table>
+				</div>
+			</div>
+			<div id="contentRight">
+				<div class="bottomBorder" id="currentTopic">
+					currentTopic
+				</div>
+				<div class="bottomBorder" id="whoIsPlaying">
+					whoIsPlaying
+				</div>
+				<div class="bottomBorder" id="chatArea">
+					chatArea
+				</div>
+				<div id="chatAreaInput">
+					chatAreaInput
+				</div>
+			</div>
+		</div>
+
+		<script src="//connect.facebook.net/en_US/all.js" type="text/javascript"></script>
+		<script src="assets/javascript/facebook.js" type="text/javascript"></script>
+	</body>
+
+</html>
+
+
+<!--
+	Team Jiminy Cricket
+-->
